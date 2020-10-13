3dc4252748cffcdc9992d1294388626194b57732
Konay <mr.mr.naymyoaung@gmail.com>
2020-10-13 19:09:51 +0630

    just backup code

--- src/service/notes.js
+++ src/service/notes.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
   const nonExisting = {
-    id: 10000,
+    id: 1000,
     content: 'This note is not saved to server',
     date: '2019-05-30T17:30:31.098Z',
     important: true,
